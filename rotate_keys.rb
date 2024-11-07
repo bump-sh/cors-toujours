6c884194e4d88d793d32d230187c004d120e225d
Manu de Bump <emmanuel@bump.sh>
2024-11-07 11:19:51 +0100

    Add GH workflow

--- rotate_keys.rb
+++ rotate_keys.rb
@@ -12,13 +12,11 @@ rsa_key = OpenSSL::PKey::RSA.new(key_size)
 puts "Private Key:"
 private_key = rsa_key.to_pem
 puts private_key
-`sed -i '' '/JWT_SIGNING_PRIVATE_KEY/d'  ./.env`
 `echo 'JWT_SIGNING_PRIVATE_KEY="#{private_key}"' >> ./.env`
 
 # Display the public key in PEM format
 puts "\nPublic Key:"
 public_key =  rsa_key.public_key.to_pem
 puts public_key
-`sed -i '' '/JWT_SIGNING_PUBLIC_KEY/d'  ./.env`
 `echo 'JWT_SIGNING_PUBLIC_KEY="#{public_key}"' >> ./.env`
 
